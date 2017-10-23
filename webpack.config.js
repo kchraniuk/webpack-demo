6a0161513f14b26ba6ef517568489a2ddff91f28
Kamil <k.chraniuk@10g.pl>
2017-10-23 23:16:46 +0200

    test 1

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const extractSASS = new ExtractTextPlugin({
-  filename: '[name].css?[hash]', 
+  filename: 'assets/css/[name].css?[hash]', 
   allChunks: true,
   disable: false
 });
@@ -11,14 +11,14 @@ const cleanPlugin = new CleanWebpackPlugin(['dist']);
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: {
-    'app.js':'./app.js',
-    'assets/css/b': './js/entry-styles.js'
+  entry: [
+    './app.js'
+    // 'assets/css/b': './js/entry-styles.js'
     // 'assets/css/app.css': './stylesheets/app.scss'
     // 'assets/css/example.css': './stylesheets/example.scss',
-  },
+  ],
   output: {
-    filename: '[name]?[hash]',
+    filename: '[name].js?[hash]',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
